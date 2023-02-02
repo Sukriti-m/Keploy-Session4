e7ac1e1742b4efa234ad8d4eb3741ce977050639
Sukriti <sukritimaurya.fzd@gmail.com>
2023-02-02 17:21:17 +0530

    call

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express=require("express");
 const app=express();
 const fs=require("fs");
+const path = require('path');
+
 app.get("/", (req,res)=>{
-    res.send("Hi, the API is working");
+    res.sendFile(path.join(__dirname+'/public/index.html'));
 
 });
 app.get("/capitals",(req,res)=>{
 fs.readFile(__dirname+'/capital.json',(err,data)=>{console.log(err);
     res.end(data);});
 });
-
 app.listen(3000,(err)=>{if(!err)
     console.log("Server started");
     });
